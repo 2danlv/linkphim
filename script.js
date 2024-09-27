1f71929dd9f87731dd6624777e7a8a5d8b2cee2e
2danlv <5029016+2danlv@users.noreply.github.com>
2024-09-28 00:39:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,176 +1,171 @@
 const itemsPerPage = 10;
-        const pageLimit = 5;
-        let currentPage = 1;
-        let data = [];
-        let filteredData = [];
-        let debounceTimeout;
+const pageLimit = 5;
+let currentPage = 1;
+let data = [];
+let filteredData = [];
+let debounceTimeout;
 
-        function fetchData(url) {
-            $('#loading').show();
-            $.getJSON(url, function(response) {
-                data = response;
-                filteredData = data;
-                currentPage = 1;
-                renderItems(currentPage);
-                renderPagination();
-            }).fail(function() {
-                console.error('Failed to fetch data from', url);
-            }).always(function() {
-                $('#loading').hide();
-            });
-        }
+function fetchData(url) {
+    $('#loading').show();
+    $('#content').hide();
+    $.getJSON(url, function(response) {
+        data = response;
+        filteredData = data;
+        currentPage = 1;
+        renderItems(currentPage);
+        renderPagination();
+    }).fail(function() {
+        console.error('Failed to fetch data from', url);
+    }).always(function() {
+        $('#loading').hide();
+        $('#content').show();
+    });
+}
 
-        function renderItems(page) {
-            const startIndex = (page - 1) * itemsPerPage;
-            const endIndex = startIndex + itemsPerPage;
-            const itemsToShow = filteredData.slice(startIndex, endIndex);
+function renderItems(page) {
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const itemsToShow = filteredData.slice(startIndex, endIndex);
 
-            const contentDiv = document.getElementById('content');
-            contentDiv.innerHTML = '';
-            itemsToShow.forEach(item => {
-                const itemList = document.createElement('div');
-                itemList.className = 'row mb-4';
-                itemList.innerHTML = `
-                    <div class="col-md-3">
-                        <img src="${item.info.poster}" class="thumb-image" alt="${item.name}">
-                    </div>
-                    <div class="col-md-9 mb-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">${item.name}</h5>
-                                <p class="card-text">${item.info.plot}</p>
-                                <div class="list-group list-group-flush">
-                                    ${item.seasons.map(season => `
-                                        <div class="list-group-item">
-                                            <h6>Season ${season.season}</h6>
-                                            ${season.episodes.map(episode => `
-                                                <div class="mb-2">
-                                                    Episode ${episode.episode}: <a href="${episode.video}" class="btn btn-primary btn-sm" target="_blank">${episode.name}</a>
-                                                </div>
-                                            `).join('')}
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = '';
+    itemsToShow.forEach(item => {
+        const itemList = document.createElement('div');
+        itemList.className = 'row mb-4';
+        itemList.innerHTML = `
+            <div class="col-md-3">
+                <img src="${item.info.poster}" class="thumb-image" alt="${item.name}">
+            </div>
+            <div class="col-md-9 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text">${item.info.plot}</p>
+                        <div class="list-group list-group-flush">
+                            ${item.seasons.map(season => `
+                                <div class="list-group-item">
+                                    <h6>Season ${season.season}</h6>
+                                    ${season.episodes.map(episode => `
+                                        <div class="mb-2">
+                                            Episode ${episode.episode}: <a href="${episode.video}" class="btn btn-primary btn-sm" target="_blank">${episode.name}</a>
                                         </div>
                                     `).join('')}
                                 </div>
-                            </div>
+                            `).join('')}
                         </div>
                     </div>
-                `;
-                contentDiv.appendChild(itemList);
-            });
+                </div>
+            </div>
+        `;
+        contentDiv.appendChild(itemList);
+    });
+}
+
+function renderPagination() {
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+    const paginationUl = document.getElementById('pagination');
+    paginationUl.innerHTML = '';
+
+    const prevLi = document.createElement('li');
+    prevLi.className = `page-item ${currentPage === 1 ? 'disabled' : ''}`;
+    prevLi.innerHTML = `<a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>`;
+    prevLi.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (currentPage > 1) {
+            currentPage--;
+            renderItems(currentPage);
+            renderPagination();
+            smoothScroll();
         }
+    });
+    paginationUl.appendChild(prevLi);
 
-        function renderPagination() {
-            const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-            const paginationUl = document.getElementById('pagination');
-            paginationUl.innerHTML = '';
+    let startPage = Math.max(1, currentPage - Math.floor(pageLimit / 2));
+    let endPage = Math.min(totalPages, startPage + pageLimit - 1);
 
-            // Previous button
-            const prevLi = document.createElement('li');
-            prevLi.className = `page-item ${currentPage === 1 ? 'disabled' : ''}`;
-            prevLi.innerHTML = `<a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>`;
-            prevLi.addEventListener('click', (event) => {
-                event.preventDefault();
-                if (currentPage > 1) {
-                    currentPage--;
-                    renderItems(currentPage);
-                    renderPagination();
-                    smoothScroll();
-                }
-            });
-            paginationUl.appendChild(prevLi);
+    if (endPage - startPage < pageLimit - 1) {
+        startPage = Math.max(1, endPage - pageLimit + 1);
+    }
 
-            // Calculate the range of page numbers to display
-            let startPage = Math.max(1, currentPage - Math.floor(pageLimit / 2));
-            let endPage = Math.min(totalPages, startPage + pageLimit - 1);
+    for (let i = startPage; i <= endPage; i++) {
+        const li = document.createElement('li');
+        li.className = `page-item ${i === currentPage ? 'active' : ''}`;
+        li.innerHTML = `<a class="page-link" href="#">${i}</a>`;
+        li.addEventListener('click', (event) => {
+            event.preventDefault();
+            currentPage = i;
+            renderItems(currentPage);
+            renderPagination();
+            smoothScroll();
+        });
+        paginationUl.appendChild(li);
+    }
 
-            // Adjust startPage if not enough pages
-            if (endPage - startPage < pageLimit - 1) {
-                startPage = Math.max(1, endPage - pageLimit + 1);
-            }
-
-            // Page numbers
-            for (let i = startPage; i <= endPage; i++) {
-                const li = document.createElement('li');
-                li.className = `page-item ${i === currentPage ? 'active' : ''}`;
-                li.innerHTML = `<a class="page-link" href="#">${i}</a>`;
-                li.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    currentPage = i;
-                    renderItems(currentPage);
-                    renderPagination();
-                    smoothScroll();
-                });
-                paginationUl.appendChild(li);
-            }
-
-            // Next button
-            const nextLi = document.createElement('li');
-            nextLi.className = `page-item ${currentPage === totalPages ? 'disabled' : ''}`;
-            nextLi.innerHTML = `<a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>`;
-            nextLi.addEventListener('click', (event) => {
-                event.preventDefault();
-                if (currentPage < totalPages) {
-                    currentPage++;
-                    renderItems(currentPage);
-                    renderPagination();
-                    smoothScroll();
-                }
-            });
-            paginationUl.appendChild(nextLi);
+    const nextLi = document.createElement('li');
+    nextLi.className = `page-item ${currentPage === totalPages ? 'disabled' : ''}`;
+    nextLi.innerHTML = `<a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>`;
+    nextLi.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderItems(currentPage);
+            renderPagination();
+            smoothScroll();
         }
+    });
+    paginationUl.appendChild(nextLi);
+}
 
-        function smoothScroll() {
-            $('html, body').animate({ scrollTop: 0 }, 500);
-        }
+function smoothScroll() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+}
 
-        $(document).ready(function() {
-            $('input[name="jsonFile"]').on('change', function() {
-                const selectedUrl = $(this).val();
-                fetchData(selectedUrl);
-                $('.fixed-search').show();
-            });
+$(document).ready(function() {
+    $('input[name="jsonFile"]').on('change', function() {
+        const selectedUrl = $(this).val();
+        fetchData(selectedUrl);
+        $('.fixed-search').show();
+    });
 
-            $('#searchInput').on('input', function() {
-                clearTimeout(debounceTimeout);
-                debounceTimeout = setTimeout(() => {
-                    const query = $(this).val().toLowerCase();
-                    filteredData = data.filter(item => item.name.toLowerCase().includes(query));
+    $('#searchInput').on('input', function() {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+            const query = $(this).val().toLowerCase();
+            filteredData = data.filter(item => item.name.toLowerCase().includes(query));
+            currentPage = 1;
+            renderItems(currentPage);
+            renderPagination();
+            smoothScroll();
+            showSuggestions(query);
+        }, 300);
+    });
+
+    function showSuggestions(query) {
+        const suggestionsDiv = $('#suggestions');
+        suggestionsDiv.empty();
+        if (query.length > 0) {
+            const suggestions = data.filter(item => item.name.toLowerCase().includes(query));
+            suggestions.forEach(suggestion => {
+                const suggestionItem = $('<div>').addClass('suggestion-item').text(suggestion.name);
+                suggestionItem.on('click', function() {
+                    $('#searchInput').val(suggestion.name);
+                    filteredData = data.filter(item => item.name.toLowerCase().includes(suggestion.name.toLowerCase()));
                     currentPage = 1;
                     renderItems(currentPage);
                     renderPagination();
-                    smoothScroll();
-                    showSuggestions(query);
-                }, 300); // Debounce for 300ms
-            });
-
-            function showSuggestions(query) {
-                const suggestionsDiv = $('#suggestions');
-                suggestionsDiv.empty();
-
-                if (query.length > 0) {
-                    const suggestions = data.filter(item => item.name.toLowerCase().includes(query));
-                    suggestions.forEach(suggestion => {
-                        const suggestionItem = $('<div>').addClass('suggestion-item').text(suggestion.name);
-                        suggestionItem.on('click', function() {
-                            $('#searchInput').val(suggestion.name);
-                            filteredData = data.filter(item => item.name.toLowerCase().includes(suggestion.name.toLowerCase()));
-                            currentPage = 1;
-                            renderItems(currentPage);
-                            renderPagination();
-                            suggestionsDiv.hide();
-                        });
-                        suggestionsDiv.append(suggestionItem);
-                    });
-
-                    suggestionsDiv.show();
-                } else {
                     suggestionsDiv.hide();
-                }
-            }
-
-            $(document).on('click', function(event) {
-                if (!$(event.target).closest('#suggestions, #searchInput').length) {
-                    $('#suggestions').hide();
-                }
+                });
+                suggestionsDiv.append(suggestionItem);
             });
-        });
+            suggestionsDiv.show();
+        } else {
+            suggestionsDiv.hide();
+        }
+    }
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#suggestions, #searchInput').length) {
+            $('#suggestions').hide();
+        }
+    });
+});
